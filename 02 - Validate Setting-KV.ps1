c27510bcf07d91f284d5d7828b925f6c0693e043
James Ng <70497056+james-ng4321@users.noreply.github.com>
2022-09-21 10:07:04 +0800

    Add files via upload

--- 02 - Validate Setting-KV.ps1
+++ 02 - Validate Setting-KV.ps1
@@ -1,7 +1,9 @@
 ﻿# Make sure you are authenticated with 
 # Connect-AzAccount
 #Connect-AzAccount -TenantId 7f2c1900-9fd4-4b89-91d3-79a649996f0a
-
+# LA workspace ID
+[string]$WorkspaceResourceID = 'aabecabb-3f4e-4d65-8a7c-17434a094cb6'
+[string]$WorkspaceResourceID2 = 'f486ca19-3cb9-4345-8639-7ccfe1fe149a'
 # CSV file
 $inputCsvFile = "HK-kv.csv"
 
@@ -21,7 +23,7 @@ $Results2 = @()
  foreach ( $Row in $SAlist )
 {
 $Results = Get-AzKeyVault -name $Row.AccountName
-
+$WorkspaceResourceID3 = Get-AzDiagnosticSetting -ResourceId $Results.ResourceId
 $Results2 += New-Object PSObject -property @{ 
 ResourceName = $Results.VaultName
 ResourceGroup = $Results.ResourceGroupName
@@ -29,6 +31,7 @@ Location = $Results.Location
 ResourceID = $Results.ResourceId
 Tags = $Results.TagsTable
 #PublicNetworkAccess = $Results.PublicNetworkAccess
+DiagnosticSetting = if(($WorkspaceResourceID -eq $WorkspaceResourceID3 )-or($WorkspaceResourceID2 -eq $WorkspaceResourceID3)){'True'}else{'False'}
 WhitelistIP = $Results.NetworkAcls.IpAddressRangesText
 AllowPublicAccess = if(($Results.PublicNetworkAccess -eq 'Enabled')-and($Results.NetworkAcls.DefaultAction -eq 'Allow')){'True'}else{''}
 RestrictAccess = if(($Results.PublicNetworkAccess -eq 'Enabled') -and ($Results.NetworkAcls.DefaultAction -eq 'Allow')){'True'}else{''}
@@ -36,6 +39,6 @@ DisableAccess = if(($Results.PublicNetworkAccess -eq "Disable")-and ($Results.Ne
 }
 }
 $path= ".\02 - Validate Result-KV.CSV"
-$Results2 | select ResourceName,ResourceGroup,ResourceID,Location,Tags,AllowPublicAccess,ResrictAccess,DisableAccess,WhitelistIP | Export-Csv -Path $path -NoTypeInformation
+$Results2 | select ResourceName,ResourceGroup,ResourceID,Location,DiagnosticSetting,AllowPublicAccess,RestrictAccess,DisableAccess,WhitelistIP,Tags | Export-Csv -Path $path -NoTypeInformation
 echo "02 - Validate Result-KV.CSV is generated !"
 
